219a326d85dca93c51083d6f3216222087aa7aa3
Lalit Singh <lalit284546@gmail.com>
2024-06-01 18:45:47 +0530

    set pages for public and private routes ,

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 function App() {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* public routes */}
-        <Route path="/sign-in" element={<SignInForm />} />
+        <Route element={<AuthLayout />}>
+          {/* public routes */}
 
-        {/* private Routes */}
-        <Route index element={<Home />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          {/* private Routes */}
+
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
